9afc169df1a0eaae09e954144805d74b6da74bfd
Ivan Solomichev <vanijatko.social@gmail.com>
2020-07-01 20:24:25 +0300

    cf finished

--- src/blockchain/managers/blockchainManager/blockchainManager_ethereum.js
+++ src/blockchain/managers/blockchainManager/blockchainManager_ethereum.js
@@ -37,7 +37,7 @@ const BlockchainManager_ethereum = {
          * Ropsten = 3
          * Kovan = 42
          */
-        if (ethereum.networkVersion != "3") {
+        if (ethereum.networkVersion != "5777") {
           throw new Error("Wrong Network. Please use Ganache for testing. Change in BlockchainManager_ethereum -> connectToMetaMask")
         }
       } catch (error) {
